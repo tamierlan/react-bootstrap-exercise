b5b3af30a0ba0649334119ed3ad9d496132e0e4f
tamierlan <hudojnik.1993@gmail.com>
2021-04-22 00:02:50 -0400

    dfgdfgdfg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card style={{color: '#000'}}>
+          <Card.Img src='https://vistapointe.net/images/card-wallpaper-15.jpg' />
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              this is the exemple of bootstrap card
+            </Card.Text>
+            <Button variant='primary'>Read More</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+          <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert varient='success'>Alert Button</Alert>
+        <Button>Button</Button>
       </header>
     </div>
   );

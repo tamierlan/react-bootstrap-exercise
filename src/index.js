5b657769e8609f3a4b00cb472b8968bee7a22ff5
tamierlan <hudojnik.1993@gmail.com>
2021-04-24 23:25:15 -0400

    adgdsf

--- src/index.js
+++ src/index.js
@@ -6,41 +6,34 @@ import reportWebVitals from './reportWebVitals';
 
 import { createStore } from 'redux';
 
-// action increment
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
+
+// action
+const increment = () => {return { type: '+' }}
+const decrement = () => {return { type: '-' }}
+
 // reducer
 const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
+  if (action.type === '+') return state + 1
+  if (action.type === '-') return state - 1
 }
+
+let store = createStore(counter)
 // display it in the console
+
+
 store.subscribe(() => console.log(store.getState()))
 
 // dispatch
-store.dispatch(increment())
+store.dispatch({type: '+'})
+
+
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

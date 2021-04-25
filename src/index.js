bf865550966d9037d9ff521644869dbee765198f
tamierlan <hudojnik.1993@gmail.com>
2021-04-24 23:01:47 -0400

    sdggdf

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+
+// action increment
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+// reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+// display it in the console
+store.subscribe(() => console.log(store.getState()))
+
+// dispatch
+store.dispatch(increment())
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
